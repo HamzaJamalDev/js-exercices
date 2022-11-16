f93c5cadbf9f04eeb3f93e8686307f19dc044d64
Hamza Jamal <hamzajamal.phd@gmail.com>
2022-11-16 23:23:35 +0100

    conditions et boucles : solution d'exercice 4

--- filejs.js
+++ filejs.js
@@ -22,13 +22,13 @@ if (p < 0) {
 }
 
 alert(message); */
-// Exercice 3
+/* Exercice 3
 let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
 let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
 let num3 = parseInt(prompt("Entrez un nombre (num3)", 0));
 let message = "";
 
-if (num1 < num2 && num1 < num3){
+if (num1 < num2 && num1 < num3) {
     message += num1 + ", ";
     if (num2 < num3) {
         message += num2 + ", " + num3;
@@ -51,4 +51,25 @@ if (num1 < num2 && num1 < num3){
     }
 }
 
-alert(message);
+alert(message);*/
+// Exercice 4
+let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
+let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
+let num3 = parseInt(prompt("Entrez un nombre (num3)", 0));
+let num4 = parseInt(prompt("Entrez un nombre (num4)", 0));
+let num5 = parseInt(prompt("Entrez un nombre (num5)", 0));
+let max;
+
+if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
+    max = num1;
+} else if (num2 > num3 && num2 > num4 && num2 > num5) {
+    max = num2;
+} else if (num3 > num4 && num3 > num5) {
+    max = num3;
+} else if (num4 > num5) {
+    max = num4;
+} else {
+    max = num5;
+}
+
+alert(max);
