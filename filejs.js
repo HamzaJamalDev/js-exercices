8a84fe3f949ad034b214b5ec2e307ea9817de446
Hamza Jamal <hamzajamal.phd@gmail.com>
2022-11-16 19:45:43 +0100

    conditions et boucles : solution d'exercice 1

---
+++ filejs.js
@@ -0,0 +1,10 @@
+// Conditions et Boucles
+// Exercice 1
+let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
+let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
+
+if (num1 > num2) {
+    console.log("num1 = " + num1 + " est le plus grand");
+} else {
+    console.log("num2 = " + num2 + " est le plus grand");
+}
