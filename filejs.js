dd4ce7f9265156616f7caf53e7d5c778b0a7759a
Hamza Jamal <hamzajamal.phd@gmail.com>
2022-11-16 23:13:56 +0100

    conditions et boucles : solution d'exercice 3

--- filejs.js
+++ filejs.js
@@ -8,7 +8,7 @@ if (num1 > num2) {
 } else {
     console.log("num2 = " + num2 + " est le plus grand");
 } */
-// Exercice 2
+/* Exercice 2
 let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
 let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
 let num3 = parseInt(prompt("Entrez un nombre (num3)", 0));
@@ -19,6 +19,36 @@ if (p < 0) {
     message += "-";
 } else {
     message += "+";
+}
+
+alert(message); */
+// Exercice 3
+let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
+let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
+let num3 = parseInt(prompt("Entrez un nombre (num3)", 0));
+let message = "";
+
+if (num1 < num2 && num1 < num3){
+    message += num1 + ", ";
+    if (num2 < num3) {
+        message += num2 + ", " + num3;
+    } else {
+        message += num3 + ", " + num2;
+    }
+} else if (num2 < num1 && num2 < num3) {
+    message += num2 + ", ";
+    if (num1 < num3) {
+        message += num1 + ", " + num3;
+    } else {
+        message += num3 + ", " + num1;
+    }
+} else {
+    message += num3 + ", ";
+    if (num1 < num2) {
+        message += num1 + ", " + num2;
+    } else {
+        message += num2 + ", " + num1;
+    }
 }
 
 alert(message);
