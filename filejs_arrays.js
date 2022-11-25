5ebc1f5f5b8cf139e44acbdd5907ac5e1ebedd86
Skafeendri <117597936+Skafeendri@users.noreply.github.com>
2022-11-25 22:23:46 +0100

    New commit added
    Exercice : Tableaux
    questions : 2 & 3

--- filejs_arrays.js
+++ filejs_arrays.js
@@ -1,15 +1,32 @@
 //Exercice 1
-
-function PlusGrand(tableau, nombre)
-{
-    let output = []
-    for(let i of tableau)
-    {
-        if(i > nombre)
-            output.push(i)
+function PlusGrand(tableau, nombre) {
+    let output = [];
+    for (let i of tableau) {
+        if (i > nombre)
+            output.push(i);
     }
-    return outp
+    return output;
 }
 
-for(let i of PlusGrand([1,2,3,4,5,6,7,8,9], 3))
-    console.log(i); // Afficher en dehors de la fonction
+for (let i of PlusGrand([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
+console.log(i); // Afficher en dehors de la fonction
+
+//Question 2 et 3 :Tableaux
+let tab = [15, 4, 22, 24, 17, 80];
+let n = prompt("Entrer un nombre", 1);
+
+//Question 2
+function Premier_Elements(tab, n) {
+    for (let i = 0; i < n; i++) { console.log(tab[i]); }
+}
+
+Premier_Elements(tab, n);
+
+//Question 3
+function Dernier_Elements(tab, n) {
+    for (let i = tab.length - n; i < tab.length; i++) {
+        console.log(tab[i]);
+    }
+}
+
+Dernier_Elements(tab, n);
