9c8d027e7f0465c344b4940faf00f0c7e15d1d77
MEDAFKARI <AFKARI_MOHAMED@emsi-edu.ma>
2022-12-01 23:08:40 +0100

    Solution exo 4 objet

--- filejs_conditions_loops.js
+++ filejs_conditions_loops.js
@@ -1,105 +1,28 @@
-// Conditions et Boucles
-/* Exercice 1
-let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
-let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
+//exo 4
 
-if (num1 > num2) {
-    console.log("num1 = " + num1 + " est le plus grand");
-} else {
-    console.log("num2 = " + num2 + " est le plus grand");
-} */
-/* Exercice 2
-let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
-let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
-let num3 = parseInt(prompt("Entrez un nombre (num3)", 0));
-let p = num1 * num2 * num3;
-let message = "Le signe du produit est : ";
-
-if (p < 0) {
-    message += "-";
-} else {
-    message += "+";
+function Personne(n, a, m) {
+    this.nom = n;
+    this.age = a;
+    this.mail = m;
 }
 
-alert(message); */
-/* Exercice 3
-let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
-let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
-let num3 = parseInt(prompt("Entrez un nombre (num3)", 0));
-let message = "";
+    Personne.prototype.Search = function(x){
+        for(let item in this)    
+            if( item == x)
+            {
+                return 1;
+                break;
+            } 
+    };
 
-if (num1 < num2 && num1 < num3) {
-    message += num1 + ", ";
-    if (num2 < num3) {
-        message += num2 + ", " + num3;
-    } else {
-        message += num3 + ", " + num2;
-    }
-} else if (num2 < num1 && num2 < num3) {
-    message += num2 + ", ";
-    if (num1 < num3) {
-        message += num1 + ", " + num3;
-    } else {
-        message += num3 + ", " + num1;
-    }
-} else {
-    message += num3 + ", ";
-    if (num1 < num2) {
-        message += num1 + ", " + num2;
-    } else {
-        message += num2 + ", " + num1;
-    }
-}
+let aziz = new Personne(['aziz', 'ahmed'], 21, 'aziz.ahmed@emsi.ma');
 
-alert(message);*/
-/* Exercice 4
-let num1 = parseInt(prompt("Entrez un nombre (num1)", 0));
-let num2 = parseInt(prompt("Entrez un nombre (num2)", 0));
-let num3 = parseInt(prompt("Entrez un nombre (num3)", 0));
-let num4 = parseInt(prompt("Entrez un nombre (num4)", 0));
-let num5 = parseInt(prompt("Entrez un nombre (num5)", 0));
-let max;
 
-if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
-    max = num1;
-} else if (num2 > num3 && num2 > num4 && num2 > num5) {
-    max = num2;
-} else if (num3 > num4 && num3 > num5) {
-    max = num3;
-} else if (num4 > num5) {
-    max = num4;
-} else {
-    max = num5;
-}
+console.log(aziz.Search('mail'));
 
-alert(max);*/
-/* Exercice 5
-for (let i = 0; i <= 15; i++) {
-    if (i % 2 === 0)
-        console.log(i + " est pair")
-    else
-        console.log(i + " est impair")
-}*/
-/* Exercice 6
-for (let i = 1; i < 101; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("multiple de trois et cinq");
-    }
-    else if (i % 5 === 0) {
-        console.log("multiple de cinq");
-    }
-    else if (i % 3 === 0) {
-        console.log("multiple de trois");
-    }
-    else {
-        console.log(i);
-    }
-}*/
-// Exercice 7
-for (let i = 1; i < 6; i++) {
-    let s = "";
-    for (let j = 0; j < i; j++) {
-        s += "*";
-    }
-    console.log(s);
-}
+
+
+
+
+    
+
