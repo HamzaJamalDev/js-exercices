92162d15c54e8117909a5461d7e6d8f81272877e
MEDAFKARI <AFKARI_MOHAMED@emsi-edu.ma>
2022-12-02 21:58:48 +0100

    New file For objects

---
+++ filejs_objects.js
@@ -0,0 +1,28 @@
+//exo 4
+
+function Personne(n, a, m) {
+    this.nom = n;
+    this.age = a;
+    this.mail = m;
+}
+
+    Personne.prototype.Search = function(x){
+        for(let item in this)    
+            if( item == x)
+            {
+                return 1;
+                break;
+            } 
+    };
+
+let aziz = new Personne(['aziz', 'ahmed'], 21, 'aziz.ahmed@emsi.ma');
+
+
+console.log(aziz.Search('mail'));
+
+
+
+
+
+    
+
