5f9d745b29a8691c6fd423aaceabe7f543629f86
othmane <othmane>
2022-12-02 21:28:06 +0100

    java ex4 objets

---
+++ filejs_objets.js
@@ -0,0 +1,26 @@
+//ex4 objet
+function Etudiant(n,p,a,no)
+{
+    this.nom=n;
+    this.age=a;
+    this.prenom=p;
+    this.note=no;
+
+}
+Etudiant.prototype.prop = function(){
+    let proto = prompt();
+    let c=0;
+ for(let i in this)
+ {
+    if(i===proto) {c++; break;}
+     
+ }
+if(c!=0) console.log("il se trouve");
+else return console.log("ne se trouve pas");
+};
+
+let othmane=new Etudiant('badaoui','othmane',20,20);
+//console.log(othmane);
+console.log(othmane);
+othmane.prop();
+
