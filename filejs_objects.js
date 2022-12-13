5d7fba8a0f301b9949f1003e624632202c38b8db
Hamza Jamal <hamzajamal.phd@gmail.com>
2022-12-13 12:13:34 +0100

    solution d'exercice 2 des objets

--- filejs_objects.js
+++ filejs_objects.js
@@ -1,28 +1,18 @@
-//exo 4
-
+//Exercice 2 (première partie)
 function Personne(n, a, m) {
     this.nom = n;
     this.age = a;
     this.mail = m;
 }
 
-    Personne.prototype.Search = function(x){
-        for(let item in this)    
-            if( item == x)
-            {
-                return 1;
-                break;
-            } 
-    };
+Personne.prototype.exist = function (x) {
+    for (let item in this) {
+        if (item == x) {
+            return true;
+        }
+    }
+    return false;
+};
 
 let aziz = new Personne(['aziz', 'ahmed'], 21, 'aziz.ahmed@emsi.ma');
-
-
-console.log(aziz.Search('mail'));
-
-
-
-
-
-    
-
+console.log(aziz.exist('mail'));
