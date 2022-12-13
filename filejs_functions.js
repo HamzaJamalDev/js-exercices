0bf3329534d9fe18e37d7fe804c5cda250769408
BOUMMISSESofia <119710851+BOUMMISSESofia@users.noreply.github.com>
2022-12-13 19:11:57 +0100

    solution d'exercice 3 des fonctions (#26)
    * Exercice5 tableaux
    
    G4
    
    * Exercice3 Fonctions
    
    * solution d'exercice 3 des fonctions
    
    Co-authored-by: Hamza Jamal <hamzajamal.phd@gmail.com>

--- filejs_functions.js
+++ filejs_functions.js
@@ -7,6 +7,23 @@ function Max_Min(N1, N2, N3, N4, N5) {
 Max_Min(prompt("Entrez Nombre 1 :"), prompt("Entrez Nombre 2 :"), prompt("Entrez Nombre 3 :"), prompt("Entrez Nombre 4 :"),
     prompt("Entrez Nombre 5 :"));
 
+// Exercice 3
+function compteur(chaine="", lettre="") {
+    let j = 0;
+    for (let i = 0; i < chaine.length; i++) {
+        if (chaine.charAt(i) === lettre) {
+            j++;
+        }
+    }
+    return j;
+}
+let s = "Sofia";
+let l = "f";
+let c = compteur(s, l);
+
+console.log("la lettre " + l + " est répétée " + c + " fois dans la chaîne " + s);
+
+
 // Exercice 4
 function unique(chaine) {
     for (let i = 0; i < chaine.length; i++) {
@@ -22,12 +39,10 @@ let chaine = prompt("Chaine :");
 console.log(unique(chaine));
 
 // Exercice 5
-let MatrixUnitaire= function(n){
+let MatrixUnitaire = function (n) {
     let s = ""
-    for(let i = 0; i < n; i++)
-    {
-        for (let i1 = 0; i1 < n; i1++)
-        {
+    for (let i = 0; i < n; i++) {
+        for (let i1 = 0; i1 < n; i1++) {
             s += (i1 === i ? "1" : "0")
         }
         s += "\n"
