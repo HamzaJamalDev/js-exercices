f794350a69dbf777c73be65a827b288a048ca8c0
BERCHIL Idriss <106925795+clownvar@users.noreply.github.com>
2022-12-13 10:02:09 +0100

    Exercice 8 + Exercice 1 filejs_ (#2)
    * Exercice 8 + Exercice 1 filejs_
    
    * Update filejs_Tableaux.js (HamzaJamalDev)
    
    il faut aussi changer Tableau dans le for loop pour eviter undentified variable.
    
    Co-authored-by: Hamza Jamal <34237018+HamzaJamalDev@users.noreply.github.com>
    
    * Exercice 1 Tableaux + Exercice5 Fonctions
    
    Co-authored-by: BERCHIL Idriss <16048625+Jonahgay@users.noreply.github.com>
    Co-authored-by: Hamza Jamal <34237018+HamzaJamalDev@users.noreply.github.com>

---
+++ filejs_Tableaux.js
@@ -0,0 +1,15 @@
+//Exercice 1
+
+function PlusGrand(tableau, nombre)
+{
+    let output = []
+    for(let i of tableau)
+    {
+        if(i > nombre)
+            output.push(i)
+    }
+    return outp
+}
+
+for(let i of PlusGrand([1,2,3,4,5,6,7,8,9], 3))
+    console.log(i); // Afficher en dehors de la fonction
