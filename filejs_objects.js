19395382e6fe2c8ac3114f29b6f219277a3ff63e
rim wakil <119077651+rimwakil@users.noreply.github.com>
2022-12-20 19:35:08 +0100

    solution d'exercice 4 des objets (#38)
    * Create exercice
    
    * solution d'exercice 4 des objets
    
    Co-authored-by: Hamza Jamal <hamzajamal.phd@gmail.com>

--- filejs_objects.js
+++ filejs_objects.js
@@ -11,7 +11,7 @@ etudiant_1.count = function () {
 
 console.log(etudiant_1.count());
 
-// Exercice 4 (première partie)
+// Exercice 4
 function Personne(nom, age) {
     this.nom = nom;
     this.age = age;
@@ -38,9 +38,41 @@ Personne.prototype.exist2 = function (proto) {
     else return false;
 };
 
-let othmane = new Personne('badaoui othmane', 20);
-othmane.note = 20;
-console.log(othmane.exist2("note"));
+// let othmane = new Personne('badaoui othmane', 20);
+// othmane.note = 20;
+// console.log(othmane.exist2("note"));
 
-let aziz = new Personne('aziz ahmed', 21);
-console.log(aziz.exist('nom'));
+// let aziz = new Personne('aziz ahmed', 21);
+// console.log(aziz.exist('nom'));
+
+// let p = new Personne("lina", 21);
+// console.log(p.exist("age"));   // dans ce cas il va retourner true
+
+function Etudiant(nom, age, matriculation, notes, modules) {
+    Personne.call(this, nom, age); // hériter les propriétés nom et age
+    this.matriculation = matriculation;
+    this.notes = notes;
+    this.modules = modules;
+}
+
+Etudiant.prototype = Object.create(Personne.prototype); // hériter la méthode exist
+Etudiant.prototype.constructor = Etudiant;
+
+Etudiant.prototype.bonjour = function () {
+    let msg = "bonjour " + this.nom + " tu as ";
+    for (let i = 0; i < this.notes.length; i++) {
+        msg += this.notes[i] + "/20 dans " + this.modules[i] + ", ";
+    }
+
+    // ou on peut faire
+    // let msg = `bonjour ${this.nom} tu as `;
+    // for (let i = 0; i < this.notes.length; i++) {
+    //     msg += `${this.notes[i]}/20 dans ${this.modules[i]}, `;
+    // }
+
+    msg = msg.substring(0, msg.length - 2); // supprimer les caractères ", " à la fin
+    console.log(msg);
+}
+
+let e = new Etudiant("lina", 21, "3452", [14, 16, 18], ["JavaScript", "SQL", "Linux"]);
+e.bonjour();
